49ef8a3d3e4e0796cc734454019ea6b33898e468
guimaraesrodrigo <guimaraesrodrigo@gmail.com>
2017-06-21 19:38:49 -0300

    Remoção de testes do Cartola.

--- Vendas/Vendas/MainPage.xaml.cs
+++ Vendas/Vendas/MainPage.xaml.cs
@@ -12,35 +12,12 @@ namespace Vendas
     {
         public MainPage()
         {
-            InitializeComponent();
+            InitializeComponent() ;
         }
 
         private void Button_Clicked(object sender, EventArgs e)
         {
             Navigation.PushAsync(new CategoriaView());
-        }
-
-        private void Button_Clicked_1(object sender, EventArgs e)
-        {
-            //try
-            //{
-                //httpClient httpClient = new HttpClient();
-                //httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-                //var response = await httpClient.GetAsync($"{BaseUrl}/clubes").ConfigureAwait(false);
-
-                //if (response.IsSuccessStatusCode)
-                //{
-                //    var content = await response.Content.ReadAsStringAsync();
-                //    return JsonConvert.DeserializeObject<List<Clube>>(content);
-                //}
-                //else return null;
-            //}
-            //catch (Exception e)
-            //{
-            //    App.Current.MainPage.DisplayAlert("", e.InnerException.Message, "ok");
-            //    return null;
-            //}
-        }
+        }       
     }
 }
